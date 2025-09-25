278277f988b1c4351c0b1daf8997ac4438228499
ThanhF-Xuaan <le227460@gmail.com>
2025-09-25 21:51:25 +0700

    first commit

---
+++ BagStoreManagement.WepApp/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BagStoreManagement.WepApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
