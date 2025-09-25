cdd0cdc8fc8973e0a2434ced5f7d5f5cb3ccec45
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-09-25 21:19:38 +0700

    setup

---
+++ BagStore.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BagStore.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
