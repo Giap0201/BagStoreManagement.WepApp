ed3a174a35e09b966b6b1fcbe32798c3b3a7f7d2
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-10-04 10:12:11 +0700

    update-change

--- BagStore.WepApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BagStoreManagement.WepApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
