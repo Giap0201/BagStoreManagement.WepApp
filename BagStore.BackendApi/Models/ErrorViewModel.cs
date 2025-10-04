bdd73903e38db8744f0762547ce2265966f13119
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2025-10-04 10:15:15 +0700

    update_change

--- BagStore.BackendApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BagStore.BackendApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
