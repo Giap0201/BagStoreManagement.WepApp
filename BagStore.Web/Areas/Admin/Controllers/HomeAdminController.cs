19163b7bf12c20d543382ad44f076ac9498b9dc2
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2025-10-05 22:49:16 +0700

    add view admin

---
+++ BagStore.Web/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BagStore.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class HomeAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
