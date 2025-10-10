79d1fbf36ef53f5ad422f4b520da5e96aff30561
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2025-10-10 22:39:20 +0700

    quan li danh muc san pham

--- BagStore.Web/Models/DTOs/ProductCategoryCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BagStore.Web.Models.DTOs
-{
-    public class ProductCategoryCreateDTO
-    {
-        public string TenLoaiTui { get; set; }
-        public string? MoTa { get; set; }
-        public int? ThuTuHienThi { get; set; }
-    }
-}
---
+++ BagStore.Web/Repositories/Interfaces/IDanhMucLoaiTuiRepository.cs
@@ -0,0 +1,18 @@
+﻿using BagStore.Domain.Entities;
+using BagStore.Web.Models.DTOs;
+
+namespace BagStore.Web.Repositories.Interfaces
+{
+    public interface IDanhMucLoaiTuiRepository
+    {
+        Task<List<DanhMucLoaiTuiDto>> GetAllDanhMucLoaiTuiAsync();
+
+        Task<DanhMucLoaiTuiDto> GetDanhMucLoaiTuiByIdAsync(int maLoaiTui);
+
+        Task<DanhMucLoaiTuiDto> CreateAsync(DanhMucLoaiTuiDto danhMucLoaiTuiDto);
+
+        Task<DanhMucLoaiTuiDto> UpdateAsync(DanhMucLoaiTuiDto danhMucLoaiTuiDto);
+
+        Task<bool> DeleteAsync(int maLoaiTui);
+    }
+}
