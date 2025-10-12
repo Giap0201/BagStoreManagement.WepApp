b8cfdf93ea3ae9f7845bbe5453f20c8a9104dd23
ThanhF-Xuaan <le227460@gmail.com>
2025-10-12 17:00:39 +0700

    implement repositories

---
+++ BagStore.Web/Repositories/implementations/ChiTietDonHangImpl.cs
@@ -0,0 +1,22 @@
+﻿using BagStore.Data;
+using BagStore.Domain.Entities;
+using BagStore.Web.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BagStore.Web.Repositories.implementations
+{
+    public class ChiTietDonHangImpl : GenericImpl<ChiTietDonHang>, IChiTietDonHangRepository
+    {
+        public ChiTietDonHangImpl(BagStoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<ChiTietDonHang>> LayTheoDonHangAsync(int maDonHang)
+        {
+            return await _dbSet
+                .Include(ct => ct.ChiTietSanPham)
+                .Where(ct => ct.MaDonHang == maDonHang)
+                .ToListAsync();
+        }
+    }
+}
---
+++ BagStore.Web/Repositories/implementations/GenericImpl.cs
@@ -0,0 +1,52 @@
+﻿using BagStore.Data;
+using BagStore.Web.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BagStore.Web.Repositories.implementations
+{
+    public class GenericImpl<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly BagStoreDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericImpl(BagStoreDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> LayTatCaAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> LayTheoIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task ThemAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task CapNhatAsync(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public async Task XoaAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public async Task LuuAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
