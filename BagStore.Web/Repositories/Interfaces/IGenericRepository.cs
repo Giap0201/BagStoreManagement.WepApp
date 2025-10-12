5e71ba3e34178541f37be114c4ca6d4b61b15bde
ThanhF-Xuaan <le227460@gmail.com>
2025-10-12 16:38:08 +0700

    repositories/interface

---
+++ BagStore.Web/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BagStore.Web.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> LayTatCaAsync();
+        Task<T?> LayTheoIdAsync(int id);
+        Task ThemAsync(T entity);
+        Task CapNhatAsync(T entity);
+        Task XoaAsync(int id);
+        Task LuuAsync();
+    }
+}
