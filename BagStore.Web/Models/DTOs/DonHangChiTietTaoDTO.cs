bf5efd24de28291d255a5f165dd6f632ea74851f
ThanhF-Xuaan <le227460@gmail.com>
2025-10-12 20:14:22 +0700

    Views

--- BagStore.Web/Models/DTOs/DonHangChiTietTaoDTO.cs
+++ BagStore.Web/Models/DTOs/DonHangChiTietTaoDTO.cs
@@ -3,6 +3,7 @@
     public class DonHangChiTietTaoDTO
     {
         public int MaChiTietSP { get; set; }
+        public string TenSP { get; set; }      // thêm tên sản phẩm
         public int SoLuong { get; set; }
         public decimal GiaBan { get; set; }
     }
