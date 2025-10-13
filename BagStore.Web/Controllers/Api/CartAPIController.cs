6a7a97d6570b2f5a5bb50f344cb0e6fb0fa444d2
Pham Duy Nghia <nghia1909200555@gmail.com>
2025-10-13 18:34:14 +0700

    update GetCartAPI

--- BagStore.Web/Controllers/Api/CartAPIController.cs
+++ BagStore.Web/Controllers/Api/CartAPIController.cs
@@ -15,14 +15,13 @@ public class CartController : ControllerBase
     [HttpGet("getcart/{userId}")]
     public IActionResult GetCart(int userId)
     {
-        if (userId == null)
-            return BadRequest("UserId không hợp lệ");
 
         var cartItems = _cartRepo.GetCartItems(userId);
 
-        if (cartItems == null || !cartItems.Any())
+        if (cartItems == null || cartItems.Items == null || !cartItems.Items.Any())
             return NotFound("Giỏ hàng trống");
 
         return Ok(cartItems);
+
     }
 }
--- BagStore.Web/Repositories/Interfaces/ICartRepository.cs
+++ BagStore.Web/Repositories/Interfaces/ICartRepository.cs
@@ -1,9 +1,11 @@
 ﻿using BagStore.Web.Models.DTOs;
+using BagStore.Web.Models.DTOs.Responses;
 
 namespace BagStore.Web.Repositories.Interfaces
 {
     public interface ICartRepository
     {
-        List<CartItemDTO> GetCartItems(int userId);
+        CartResponse GetCartItems(int userId);
+
     }
 }
