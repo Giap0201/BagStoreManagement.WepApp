8a0695477709c33d33b6b5713ea1c183f6787f56
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2025-10-19 17:52:08 +0700

    Revert "Feature/lxt/order"

--- BagStore.Web/Repositories/Interfaces/IChiTietDonHangRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BagStore.Domain.Entities;
-
-namespace BagStore.Web.Repositories.Interfaces
-{
-    public interface IChiTietDonHangRepository : IGenericRepository<ChiTietDonHang>
-    {
-        Task<IEnumerable<ChiTietDonHang>> LayTheoDonHangAsync(int maDonHang);
-    }
-}
