61c45d1a3dc7a2d020894d3011220bf5c9ed3e04
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-10-21 00:20:06 +0700

    add api anh san pham

---
+++ BagStore.Web/Services/Interfaces/IAnhSanPhamService.cs
@@ -0,0 +1,24 @@
+﻿using BagStore.Domain.Entities;
+using BagStore.Models.Common;
+using BagStore.Web.Models.DTOs.SanPhams;
+using BagStore.Web.Models.ViewModels.SanPhams;
+
+namespace BagStore.Web.Services.Interfaces
+{
+    public interface IAnhSanPhamService
+    {
+        Task<BaseResponse<AnhSanPhamResponseDto>> CreateAsync(AnhSanPhamRequestDto dto);
+
+        Task<BaseResponse<AnhSanPhamResponseDto>> UpdateAsync(int maAnh, AnhSanPhamRequestDto dto);
+
+        Task<BaseResponse<bool>> DeleteAsync(int maAnh);
+
+        Task<BaseResponse<AnhSanPhamResponseDto>> GetByIdAsync(int maAnh);
+
+        Task<BaseResponse<List<AnhSanPhamResponseDto>>> GetBySanPhamIdAsync(int maSP);
+
+        Task<BaseResponse<List<AnhSanPhamResponseDto>>> GetAllAsync();
+
+        Task<BaseResponse<List<AnhSanPham>>> CreateMultipleAsync(int maSP, List<IFormFile> files);
+    }
+}
