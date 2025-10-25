fe106b1fecb35e965014ac908aa44d48b870979b
ngkhson <nkhanhson25112005@gmail.com>
2025-10-25 12:53:08 +0700

    add role customer

--- BagStore.Web/Program.cs
+++ BagStore.Web/Program.cs
@@ -138,6 +138,17 @@ using (var scope = app.Services.CreateScope())
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
+    string[] roleNames = { "Admin", "Customer", "Employee" };
+
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+
     if (!await roleManager.RoleExistsAsync("Admin"))
         await roleManager.CreateAsync(new IdentityRole("Admin"));
 
