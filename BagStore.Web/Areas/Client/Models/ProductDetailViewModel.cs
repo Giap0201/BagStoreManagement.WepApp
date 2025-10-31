6b82282a60c4368f3d034528162c810574c2be57
Dang Trung <trungnd3605@gmail.com>
2025-10-31 23:59:09 +0700

    update detail-sp

--- BagStore.Web/Areas/Client/Models/ProductDetailViewModel.cs
+++ BagStore.Web/Areas/Client/Models/ProductDetailViewModel.cs
@@ -5,9 +5,9 @@ namespace BagStore.Web.Areas.Client.Models
 {
     public class ProductDetailViewModel
     {
-        public ChiTietSanPham Variant { get; set; } = null!;         // variant currently shown (MaChiTietSP)
-        public SanPham? Product => Variant?.SanPham;
+        public ChiTietSanPham Variant { get; set; }
         public List<ChiTietSanPham> AllVariants { get; set; } = new();
         public List<AnhSanPham> Images { get; set; } = new();
+        public SanPham Product => Variant?.SanPham;
     }
 }
