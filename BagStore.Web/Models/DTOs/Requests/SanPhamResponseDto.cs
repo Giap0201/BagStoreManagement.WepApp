9149fa4d010f22d808401d4b569060f86de335d5
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-11-04 00:14:14 +0700

    update final version 1

---
+++ BagStore.Web/Models/DTOs/Requests/SanPhamResponseDto.cs
@@ -0,0 +1,28 @@
+﻿namespace BagStore.Web.Models.DTOs.Requests
+{
+    public class SanPhamResponseDto
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; } = string.Empty;
+
+        public string? MoTaChiTiet { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+
+        public int MaLoaiTui { get; set; }
+        public string? TenLoaiTui { get; set; }
+
+        public int MaThuongHieu { get; set; }
+        public string? TenThuongHieu { get; set; }
+
+        public int MaChatLieu { get; set; }
+        public string? TenChatLieu { get; set; }
+
+        public decimal? GiaBan { get; set; }
+
+        public string? AnhChinh { get; set; } // URL ảnh chính
+
+        public DateTime? NgayCapNhap { get; set; }
+        public int? MaChiTietSP { get; set; }
+    }
+}
